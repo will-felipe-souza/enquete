c04c0bb8a4d0bbe33cf153301ce6ff0f62b5ecb6
will-felipe-souza <will.felipe.souza@gmail.com>
2025-05-08 20:38:34 -0300

    Subindo teste erro prisma

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type { NextConfig } from "next"
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin")
+/* eslint-enable @typescript-eslint/no-require-imports */
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,6 +9,12 @@ const nextConfig: NextConfig = {
     domains: ["utfs.io"],
   },
   serverExternalPackages: ["@prisma/client"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
 }
 
 export default nextConfig
